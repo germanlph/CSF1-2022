6995c0ddc24af565207251584da6e6b637a1402d
German Portillo <germanlph@gmail.com>
2022-03-23 18:59:26 -0500

    Data types lab

--- _2Tuesday/DataTypesLab.cs
+++ _2Tuesday/DataTypesLab.cs
@@ -26,7 +26,33 @@ namespace _2Tuesday
              * least one whole number type and display them.
              */
 
-            Console.WriteLine("Something on this \n line");
+            string location = "Kansas";
+            string weather = "lame";
+            int days = 2;
+            int temp = 72;
+            decimal symptom = 101.5m;
+            bool feelingsOnTheMatter = true;
+            bool wantItToChange = true;
+            char badGrade = 'F';
+            char passingGrade = 'C';
+            char topGrade = 'A';
+
+            Console.WriteLine("I live in " + location + " and the weather here right now is " + weather);
+            Console.WriteLine("In fact, it's been " + weather + " for " + days + " days");
+            Console.WriteLine("I wish it were currently " + temp + " degrees, because this " + weather + " weather makes me " +
+                " as sick as a " + symptom + " fever");
+            Console.WriteLine("Do I like the ideas of seasons? " + feelingsOnTheMatter);
+            Console.WriteLine("Do I still wish spring would hurry up? " + wantItToChange);
+            Console.WriteLine("So I will give this current weather a grade of " + badGrade);
+            Console.WriteLine("I'd be happy with weather that was about on the level of a " + passingGrade);
+            Console.WriteLine("But what I really want is grade " + topGrade + " weather");
+
+
+            Console.WriteLine("The maximum of an int is: " + Int32.MaxValue); // What happens after I add a period?*******
+            Console.WriteLine("The minimum value of a byte is: " + byte.MinValue); // Framework Class Library
+
+
+            Console.WriteLine("Something on this \n line.");
         }
     }
 }
