d7e189320d18105fc4cf957212062f583d95e5b3
German Portillo <germanlph@gmail.com>
2022-03-31 22:01:16 -0500

    the end of the class

---
+++ _5Friday/LoopingDoWhile.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5Friday
+{
+    class LoopingDoWhile
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Looping with DoWhile");
+            Console.Title = "---------------> Do While <------------------";
+
+            /* Do While Loop - best for when you want to run code
+             * an indeterminable number of times BUT
+             * at least once!!!
+             * (1 to ???)
+             * 
+             * COUNTER
+             * 
+             * do
+             * {
+             *      //code to run
+             *      UPDATE
+             *      
+             * } while (CONDITION);
+             */
+
+            int cookie = 1;//Counter/Initializer
+
+            do
+            {
+                Console.WriteLine("Yum! You have had " + cookie + " cookie" + (cookie == 1 ? "" : "s"));
+                cookie++;
+            } while (cookie <= 5);
+
+            // The below do while is not great. It doesn't do much other than show an example of a do while
+            // A do while will ALWAYS run at least once
+            Console.WriteLine("\n\nDo you want to run this program?");
+            string runProgram = Console.ReadLine().ToUpper();
+
+            do
+            {
+                Console.WriteLine("Success! The program is running!");
+                // Could be many lines of code here
+                Console.WriteLine("Do you want to keep going?");
+                runProgram = Console.ReadLine().ToUpper();// UPDATE
+            } while (runProgram == "Y" || runProgram == "YES"); // CONDITION
+            // Don't forget the ;
+            // The above example should have been a while loop because it may not need
+            // to run at all if the user says anything but YES from the start
+
+            // Real-world example
+            bool repeat = true; //Assume they want to run the program
+            // Generally you write a while or do while to run forever
+            // and just deal with it when they want to stop.
+            // The above bool, repeat, is our counter
+
+            do
+            {
+                // do while ALWAYS runs once, at least! Best for menus!!!
+                Console.WriteLine("-= Wekk Ant Eeru Phone Company =-");
+                Console.WriteLine("B) Bill\nP) Payment\nS) Service\nX) Exit");
+                string userChoice = Console.ReadLine().ToUpper();
+
+                // Clear the console after input
+                Console.Clear();
+
+                switch (userChoice)
+                {
+                    case "B":
+                    case "BILL":
+                        Console.WriteLine("Your balance is $500.00\n\n");
+                        break;
+
+                    case "P":
+                    case "PAYMENT":
+                        Console.WriteLine("Your payment is pending...\n\n");
+                        break;
+
+                    case "S":
+                    case "SERVICE":
+                        Console.WriteLine("We will be there in 1-30 days. Someone must be present when we arrive.\n\n");
+                        break;
+
+                    case "X":
+                    case "EXIT":
+                        Console.WriteLine("Thank you for calling.");
+                        repeat = false;// UPDATE
+                        break;
+                    default:
+                        Console.WriteLine("That was not a valid option.\nA $5 invalid option fee has been applied to your bill.");
+                        break;
+                }// end switch
+
+            } while (repeat);// Condition
+        }// end main()
+    }// end class
+}// end namespace
---
+++ _5Friday/LoopingForEach.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5Friday
+{
+    class LoopingForEach
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Looping with For Each");
+            Console.Title = @"\\\\\****** For Each ******/////";
+
+            /* FOREACH is a specialized loop made for collections.
+             * It provides READONLY access
+             * You can see the values in the collection, but you
+             * can NOT change them in a foreach.
+             * 
+             * foreach (DATATYPE MADEUPNAME in COLLECTION)
+             * {
+             *      //code to run with MADEUPNAME
+             * }
+             * 
+             * This is the easiest, least error-prone loop. 
+             * It is preferred in many program contexts,
+             * but we lose some flexibility with it.
+             * 
+             * The foreach-loop is versatile.
+             * We will end up using them in strings,
+             * string arrays, and even DataTables.
+             */
+
+            string[] colors = { "red", "blue", "green", "purple", "black", "pink" }; // inline declare and initialize array
+
+            foreach (string color in colors)
+            {
+                Console.WriteLine(color);
+                //colors[2] = "peachpuff";// allowed but a VERY BAD IDEA
+                //Don't try to change a collection while you're looping inside of it.
+            }//end foreach
+
+            Console.WriteLine();
+
+            int[] gradeScores = { 100, 80, 42, 75, 2 };
+
+            foreach (int bob in gradeScores)// the name inside the loop can we whatever I want
+            {
+                Console.WriteLine(bob);
+            }// end foreach
+
+            // MINI LAB
+            // Print out each of the prices in the
+            // below array to the screen with currency formatting:
+
+            Console.WriteLine();
+
+            decimal[] cartPrices = { 12.99m, 2, 9.99m, 10, 20 };
+
+            foreach (decimal p in cartPrices)
+            {
+                Console.WriteLine($"{p:c}");
+            }
+
+            // Let's total up all of the prices in the cart.
+            // Create a running total variable so we can 
+            // print out the total AFTER the foreach
+
+            decimal totalSale = 0;// always give a running-total variable an initial value
+
+            Console.WriteLine("\nThank you for purchasing from WeRProgrammers.Inc\n");
+
+            foreach (decimal price in cartPrices)
+            {
+                totalSale += price;
+                //totalSale = totalSale + price;//another option
+                Console.WriteLine("After adding {0:c}, the total is now: {1:c}", price, totalSale);
+            }// end foreach
+
+            Console.WriteLine("\nYour total purchase is: {0:c}", totalSale);
+
+            // Remember, a string is a collection of characters
+            string letters = "abcdefghijklmnopqrstuvwxyz";
+
+            foreach (char c in letters)
+            {
+                Console.WriteLine(c);
+            }
+
+            Console.WriteLine(letters.Length + " total letters");
+
+            //string backwards = letters.Reverse().ToString();
+
+        }
+    }
+}
